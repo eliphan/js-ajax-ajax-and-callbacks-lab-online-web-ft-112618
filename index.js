da285766d18c1e865d1d16b3e3d31ca8b0e0a609
Elise Phan <aliceconnic@gmail.com>
2019-03-21 02:20:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,6 +9,21 @@ function searchRepositories() {
     })
 }
 
+function renderSearchResult(result) {
+  return `
+      <div>
+        <h2><a href="${result.html_url}">${result.name}</a></h2>
+        <p><a href="#" data-repository="${result.name}" data-owner="${result.owner.login}" onclick="showCommits(this)">Show Commits</a></p>
+        <p>${result.description}</p>
+      </div>
+      <hr>
+    `
+}
+
+function renderSearchResults(data) {
+  data.items.map( result => renderSearchResult(result))
+}
+
 function displayError() {
   $('#errors').html("I'm sorry, there's been an error. Please try again.")
 }

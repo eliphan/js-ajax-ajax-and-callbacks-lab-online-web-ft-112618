e8fee8d5786418ca22197aaf9fc744159b188464
Elise Phan <aliceconnic@gmail.com>
2019-03-21 01:53:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 $(document).ready(function (){
 });
 
-var displayError = () => $('#errors').html("I'm sorry, there's been an error. Please try again.")
+function displayError() {
+  $('#errors').html("I'm sorry, there's been an error. Please try again.")
+}
 
 var renderCommit = (commit) => {
   return `<li><h3>${commit.sha}</h3><p>${commit.commit.message}</p></li>`
